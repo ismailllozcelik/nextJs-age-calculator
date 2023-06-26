e253bd93f1b79c556cb4a8648124f29183d99dc6
ismailllozcelik <ismailll.ozcelik@gmail.com>
2023-06-26 17:25:12 +0300

    moment js and primereact added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import 'primereact/resources/primereact.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 
 export default function Home() {
+  function clicked() {
+    console.log('tıkladı');
+  }
   return (
     <div className={styles.container}>
       <Head>
         <title>Age Calculator</title>
       </Head>
-      <div className="bg-yellow-300 flex h-screen justify-center items-center">
-        <button className="bg-white p-5 shadow-lg duration-200 hover:bg-yellow-200 transform hover:-skew-x-12">
-          Hello tailwind next
-        </button>
+      <div className="h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <div className="h-screen flex items-center justify-center">
+          <div class="bg-white p-12 rounded shadow-lg">
+            Merhaba, ben ekranın ortasındayım! <br />
+            <Button onClick={clicked} severity="success" label="Hesapla" />
+          </div>
+        </div>
       </div>
     </div>
   );
